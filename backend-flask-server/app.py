df43f7b1e51876082749586a6a7a85e7e4edd7da
Mato Gudelj <mato.gudelj@tum.de>
2023-11-18 17:09:24 +0100

    Add skeletons for backend api routes.

--- backend-flask-server/app.py
+++ backend-flask-server/app.py
@@ -13,7 +13,36 @@ def index():
 # with any possible routes used by the React side. E.g. "/api/time" instead of "/time".
 @app.route('/api/time')
 def get_current_time():
-    return {'time': time.time()}
+    return {'time': time.time()}')
+
+
+@app.route('/api/upload', methods=['POST'])
+def upload_file():
+    # TODO: Saves the log file and initiates the parsing/embedding process
+    return {'success': True}
+
+@app.route('/api/summary', methods=['POST'])
+def update_summary():
+    if request.method == 'POST':
+        request_data = request.get_json()
+        current_summary = request_data['current_summary']
+        update_message = request_data['update_message']
+
+        # TODO: Pass the current_summary and update_message to the model, which re-queries the database and returns an updated summary
+        updated_summary = current_summary  # Replace with actual update logic
+
+        return {'updated_summary': updated_summary}
+
+@app.route('/api/get_context', methods=['POST'])
+def get_context():
+    if request.method == 'POST':
+        request_data = request.get_json()
+        target_row = request_data['target_row']
+
+        # TODO: Get the surrounding window of rows from the database
+
+        return {'context': []}
+        
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
